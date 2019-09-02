441c6564f29110aaef00d66d37daba01ed4697f2
Amaro <amarito93@gmail.com>
2019-09-02 17:14:48 -0500

    Adding comments for 2nd approach

--- index.js
+++ index.js
@@ -1,43 +1,68 @@
 const http = require('http');
 const { pipeline: callbackPipeline } = require('stream');
-const { createReadStream, createWriteStream } = require('fs');
+const { createReadStream, createWriteStream, writeFile: writeCallback } = require('fs');
 const { promisify } = require('util');
+const vision = require('@google-cloud/vision');
 
 const pipeline = promisify(callbackPipeline);
+const writeFile = promisify(writeCallback);
+
+const client = new vision.ImageAnnotatorClient();
 
 async function quickstart() {
-    // Imports the Google Cloud client library
-    const vision = require('@google-cloud/vision');
-  
-    // Creates a client
-    const client = new vision.ImageAnnotatorClient();
-  
-    // Performs label detection on the image file
     console.log(Object.getOwnPropertyNames(client), Object.getPrototypeOf(client));
-    const [ result ] = await client.labelDetection('./upload.jpg');
-    console.table(result.labelAnnotations);
+
+    const [ { labelAnnotations } ] = await client.labelDetection('./upload.jpg');
+    const [ { faceAnnotations } ] = await client.faceDetection('./upload.jpg');
+
+    return { labelDetection: labelAnnotations, faceDetection: faceAnnotations };
 }
 
 http.createServer(async (request, response) => {
   const { headers, method, url } = request;
+  //let result = '';
+
+  //request.setEncoding('binary');
 
   request.on('end', async () => {
-    quickstart().catch(console.error);
+    /*try {
+      const dataBuffer = Buffer.from(result, 'binary');
+      await writeFile('./upload.jpg', dataBuffer);
+    } catch(error) {
+      console.error('Got an error', error);
+    } finally {
+      result = '';
+    }*/
 
     response.on('error', (err) => {
       console.error(err);
     });
 
-    response.statusCode = 200;
     response.setHeader('Content-Type', 'application/json');
-    // Note: the 2 lines above could be replaced with this next one:
-    // response.writeHead(200, {'Content-Type': 'application/json'})
+    response.setHeader('Access-Control-Allow-Origin', '*');
+    response.setHeader('Access-Control-Allow-Credentials', 'true');
+    response.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, DELETE');
+    response.setHeader('Access-Control-Max-Age', '3600');
+    response.setHeader('Access-Control-Allow-Headers', 'Content-Type, Accept, X-Requested-With, remember-me');
+    let responseBody = { headers, method, url };
 
-    const responseBody = { headers, method, url, body: "yes" };
+    try {
+      const result = await quickstart();
+
+      response.statusCode = 200;
+      responseBody.body = result;
+    } catch(error) {
+      response.statusCode = 500;
+      responseBody.body = error.message;
+    }
 
     response.write(JSON.stringify(responseBody));
     response.end();
   });
+
+  /*request.on('data', data => {
+    result += data;
+  });*/
 
   try {
     await pipeline(
@@ -48,4 +73,4 @@ http.createServer(async (request, response) => {
   } catch (err) {
     console.error('Pipeline failed', err);
   }
-}).listen(3000);
+}).listen(3001);
